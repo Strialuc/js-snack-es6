71cf1e7e507b4e9de3c6a2859be2e7e7c351c23a
Luca Striano <luca.striano90@gmail.com>
2025-12-15 16:10:48 +0100

    creazione file e completamento primo snack

---
+++ snack1/js/script.js
@@ -0,0 +1,53 @@
+/*Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.*/
+
+
+// richiamo l'id dell elemento presente in html 
+
+const titolo = document.getElementById("bicicletta")
+
+
+// creo l' array bicicletta 
+
+const biciclette = [];
+
+// funzione per un numero random 
+
+function numeroRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// ciclo for  per inserire peso delle biciclette
+
+for (let i = 0; i < 10; i++) {
+  const bicicletta = {
+    nome: (i),
+    peso: numeroRandom(100, 800),    
+  };
+
+  biciclette.push(bicicletta);
+}
+
+console.log(biciclette);
+
+// ciclo for per trovare il peso minore 
+
+let pesoMinimo = biciclette[0].peso;
+
+console.log(pesoMinimo);
+
+for (let index = 0; index < biciclette.length; index++) {
+
+    if (biciclette[index].peso < pesoMinimo) {
+
+        pesoMinimo = biciclette[index].peso
+    }
+}
+
+console.log(pesoMinimo);
+
+titolo.innerText = "La bicicletta più leggera pesa " + pesoMinimo;
+
+
